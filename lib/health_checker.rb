7c3f67e00a7d8e2b384453e2f25e2d973f499177
mewlist <mewlist@mewlist.com>
2012-03-21 15:26:07 +0900

    Fixes bugs

--- lib/health_checker.rb
+++ lib/health_checker.rb
@@ -6,9 +6,9 @@ class HealthChecker
   def call(env)
     if env['REQUEST_PATH'] == '/health_check'
       header ||= {}
-      [200, header, 'healty']
+      [200, header, 'healthy']
     else
-      @app.call
+      @app.call env
     end
   end
 end
--- lib/health_checker/version.rb
+++ lib/health_checker/version.rb
@@ -1,3 +1,3 @@
-module HealthChecker
-  VERSION = "0.0.2"
+class HealthChecker
+  VERSION = "0.0.3"
 end
